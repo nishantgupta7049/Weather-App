a8ed915c13d3f844b8aa643220f3f58d0ef7649e
nishantgupta7049 <www.gn7049@gmail.com>
2026-05-30 18:54:25 +0530

    Added weather app

--- Weather-App.js
+++ Weather-App.js
@@ -1,19 +1,57 @@
-let apiKey="15380ebebbbe38a332a3ce013441db70";
+let apiKey = "15380ebebbbe38a332a3ce013441db70";
 
-let searchBtn=document.querySelector("#search-btn");
-let result=document.querySelector("#result");
+let searchBtn = document.querySelector("#search-btn");
+let resultBox = document.querySelector("#result");
 
-searchBtn.addEventListener("click", async ()=>{
-    let cityName=document.querySelector("#city-name").value.trim();
+searchBtn.addEventListener("click", async () => {
+  let cityName = document.querySelector("#city-name").value.trim();
 
-    if (cityName===""){
-        result.classList.remove("hidden");
-        result.innerHTML="<h3 class=>Please enter a city name</h3>";
-        return;
-    
-}
-try{
-}catch(error){
+  if (cityName === "") {
+    resultBox.classList.remove("hidden");
+    resultBox.innerHTML = "<h3>Please enter a city name</h3>";
+    return;
+  }
+
+  try {
+    resultBox.classList.remove("hidden");
+
+    resultBox.innerHTML = `
+      <div class="flex justify-center items-center">
+        <div class="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        <p class="text-center mt-3 ms-1 text-gray-700 font-medium">Fetching Weather Details...</p>
+      </div>
+    `;
+
+    let response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
+    );
+
+    let data = await response.json();
+    console.log(data);
+
+    // weather details
+    let icon = data.weather[0].icon;
+    resultBox.innerHTML = `
+    <div>
+      <h2 class="text-3xl font-bold text-gray-800">${data.name}, ${data.sys.country}</h2>
+      <p class="text-gray-600 mt-1">Temperature: ${data.weather[0].main}</p>
+      <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="loading..." class="w-16 h-16 mt-2"/>
+      <h1 class="text-5xl font-bold text-gray-700">${data.main.temp}°C</h1>
+      
+    </div>
+
+    <div class="grid grid-cols-2 gap-4 mt-6">
+    <div class="bg-white/60 p-4 rounded-xl shadow">
+    <p class="text-gray-600 text-sm">Humidity</p>
+    <h3 class="text-2xl font-bold text-gray-800">${data.main.humidity}%</h3>
+    </div>
+    <div class="bg-white/60 p-4 rounded-xl shadow">
+    <p class="text-gray-600 text-sm">Wind Speed</p>
+    <h3 class="text-2xl font-bold text-gray-800">${data.wind.speed} m/s</h3>
+    </div>
+    `;
+  } catch (error) {
     console.error(error);
-    result.innerHTML=`<h3> Error in Fetching Weather Data...</h3>`;
-}});
+    resultBox.innerHTML = `<h3>Error in Fetching Weather Data...</h3>`;
+  }
+});
